24c4fe43424c9e97549bb3cbc59bcd6dcca6948c
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-10-31 20:37:05 +0100

    Added a test case for unsuccessful processing of messages

--- service/basic_test.go
+++ service/basic_test.go
@@ -81,3 +81,24 @@ func TestMissingAction(t *testing.T) {
 	con := service.NewConsumer(mockConn)
 	con.Listen()
 }
+
+func TestUnsuccessfulProcessing(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockMsg := test_mocks.NewMockMessage(mockCtrl)
+	mockConn := test_mocks.NewTestConnection(mockMsg, mockCtrl)
+	mockAction := test_mocks.NewMockAsynchronousAction(mockCtrl)
+
+	mockMsg.EXPECT().GetHeaderValue("action").Return("foo", nil)
+	mockAction.EXPECT().Process(mockMsg).Return(
+		errors.New("Something went wrong!"),
+	)
+
+	mockConn.EXPECT().AckMessage(mockMsg).Return(nil)
+
+	con := service.NewConsumer(mockConn)
+	con.AddAsyncAction(mockAction, "foo")
+
+	con.Listen()
+}
