1b0fc2fd5e8448f55382e3193ee36067757e2939
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-10-31 21:29:00 +0100

    Added some more test cases

--- service/action_header_error_test.go
+++ service/action_header_error_test.go
@@ -6,6 +6,7 @@ import(
 
 	"testing"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"errors"
 )
 
@@ -40,4 +41,48 @@ func TestMissingAction(t *testing.T) {
 
 	con := service.NewConsumer(mockConn)
 	con.Listen()
+}
+
+func TestAttemptingToAddSameAsyncActionTwice(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockMsg := test_mocks.NewMockMessage(mockCtrl)
+	mockConn := test_mocks.NewTestConnection(mockMsg, mockCtrl)
+
+	first := test_mocks.NewMockAsynchronousAction(mockCtrl)
+	second := test_mocks.NewMockAsynchronousAction(mockCtrl)
+
+	con := service.NewConsumer(mockConn)
+	err := con.AddAsyncAction(first, "foo")
+	assert.Equal(t, nil, err)
+
+	err = con.AddAsyncAction(second, "foo")
+	assert.Equal(
+		t,
+		errors.New("Action foo already exists in async action list"),
+		err,
+	)
+}
+
+func TestAttemptingToAddSameSyncActionTwice(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockMsg := test_mocks.NewMockMessage(mockCtrl)
+	mockConn := test_mocks.NewTestConnection(mockMsg, mockCtrl)
+
+	first := test_mocks.NewMockSynchronousAction(mockCtrl)
+	second := test_mocks.NewMockSynchronousAction(mockCtrl)
+
+	con := service.NewConsumer(mockConn)
+	err := con.AddSyncAction(first, "foo")
+	assert.Equal(t, nil, err)
+
+	err = con.AddSyncAction(second, "foo")
+	assert.Equal(
+		t,
+		errors.New("Action foo already exists in sync action list"),
+		err,
+	)
 }
