49a2646bf2fa0b5b405c1fddc8e13a67e228a28c
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-10-31 20:23:56 +0100

    Added some expectations to the test connection

--- test_mocks/test_connection.go
+++ test_mocks/test_connection.go
@@ -1,48 +1,83 @@
 package test_mocks
 
+// This one's part generated, part hand-written - it's stateful, so we need
+// to give it at least a little implementation-y goodness or else things won't
+// work right
+
 import(
 	"github.com/NGTOne/warren/conn"
 
-	"errors"
+	"github.com/golang/mock/gomock"
+	"reflect"
 )
 
 type TestConnection struct{
+	ctrl     *gomock.Controller
+	recorder *TestConnectionMockRecorder
 	callback func(conn.Message)
 
-	Message conn.Message
-	AckError bool
-	NackError bool
-	ReplyError bool
+	msg conn.Message
 }
 
-func (con *TestConnection) Listen() {
-	con.callback(con.Message)
+type TestConnectionMockRecorder struct {
+	mock *TestConnection
 }
 
-func (con *TestConnection) SetNewMessageCallback(f func(conn.Message)) {
-	con.callback = f
+func NewTestConnection(
+	msg conn.Message,
+	ctrl *gomock.Controller,
+) *TestConnection {
+	mock := &TestConnection{ctrl: ctrl, msg: msg}
+	mock.recorder = &TestConnectionMockRecorder{mock}
+	return mock
 }
 
-func (con *TestConnection) AckMessage(m conn.Message) error {
-	if (con.AckError) {
-		return errors.New("Something went wrong!")
-	}
-	return nil
+func (m *TestConnection) SetNewMessageCallback(f func(conn.Message)) {
+	m.callback = f
 }
 
-func (con *TestConnection) NackMessage(m conn.Message) error {
-	if (con.NackError) {
-		return errors.New("Something went wrong!")
-	}
-	return nil
+// Borrowed from gomock's generated code
+func (m *TestConnection) EXPECT() *TestConnectionMockRecorder {
+	return m.recorder
 }
 
-func (con *TestConnection) SendResponse(
-	original conn.Message,
-	response conn.Message,
-) error {
-	if (con.ReplyError) {
-		return errors.New("Something went wrong!")
-	}
-	return nil
+// AckMessage mocks base method
+func (m *TestConnection) AckMessage(arg0 conn.Message) error {
+	ret := m.ctrl.Call(m, "AckMessage", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// AckMessage indicates an expected call of AckMessage
+func (mr *TestConnectionMockRecorder) AckMessage(arg0 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AckMessage", reflect.TypeOf((*TestConnection)(nil).AckMessage), arg0)
+}
+
+// Listen mocks base method
+func (m *TestConnection) Listen() {
+	m.callback(m.msg)
+}
+
+// NackMessage mocks base method
+func (m *TestConnection) NackMessage(arg0 conn.Message) error {
+	ret := m.ctrl.Call(m, "NackMessage", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// NackMessage indicates an expected call of NackMessage
+func (mr *TestConnectionMockRecorder) NackMessage(arg0 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "NackMessage", reflect.TypeOf((*TestConnection)(nil).NackMessage), arg0)
+}
+
+// SendResponse mocks base method
+func (m *TestConnection) SendResponse(arg0, arg1 conn.Message) error {
+	ret := m.ctrl.Call(m, "SendResponse", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SendResponse indicates an expected call of SendResponse
+func (mr *TestConnectionMockRecorder) SendResponse(arg0, arg1 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendResponse", reflect.TypeOf((*TestConnection)(nil).SendResponse), arg0, arg1)
 }
