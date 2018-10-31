e20b241a09953ced1a2930ca0f7c635a90f75247
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-10-31 21:55:05 +0100

    Added tests for the built-in error handlers

---
+++ err_handler/acking_test.go
@@ -0,0 +1,28 @@
+package err_handler_test
+
+import(
+	"github.com/NGTOne/warren/err_handler"
+	"github.com/NGTOne/warren/test_mocks"
+
+	"testing"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"errors"
+)
+
+func TestAckingHandler(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockMsg := test_mocks.NewMockMessage(mockCtrl)
+	mockConn := test_mocks.NewTestConnection(mockMsg, mockCtrl)
+
+	mockConn.EXPECT().AckMessage(mockMsg).Return(nil)
+
+	err := errors.New("Something went wrong!")
+
+	handler := err_handler.NewAckingHandler(mockConn)
+	result := handler.ProcessError(mockMsg, err)
+
+	assert.Equal(t, err, result)
+}
