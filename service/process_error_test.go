ae3d9dcf56e357a0216259169d22df1018d1b258
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-10-31 20:48:54 +0100

    Added more error test cases

--- service/process_error_test.go
+++ service/process_error_test.go
@@ -29,3 +29,48 @@ func TestUnsuccessfulProcessing(t *testing.T) {
 
 	con.Listen()
 }
+
+func TestUnsuccessfulReply(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockMsg := test_mocks.NewMockMessage(mockCtrl)
+	mockReply := test_mocks.NewMockMessage(mockCtrl)
+	mockConn := test_mocks.NewTestConnection(mockMsg, mockCtrl)
+	mockAction := test_mocks.NewMockSynchronousAction(mockCtrl)
+
+	mockMsg.EXPECT().GetHeaderValue("action").Return("foo", nil)
+	mockAction.EXPECT().Process(mockMsg).Return(mockReply, nil)
+
+	mockConn.EXPECT().SendResponse(mockMsg, mockReply).Return(
+		errors.New("Something went wrong!"),
+	)
+	mockConn.EXPECT().AckMessage(mockMsg).Return(nil)
+
+	con := service.NewConsumer(mockConn)
+	con.AddSyncAction(mockAction, "foo")
+
+	con.Listen()
+}
+
+func TestUnsuccessfulAck(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockMsg := test_mocks.NewMockMessage(mockCtrl)
+	mockConn := test_mocks.NewTestConnection(mockMsg, mockCtrl)
+	mockAction := test_mocks.NewMockAsynchronousAction(mockCtrl)
+
+	mockMsg.EXPECT().GetHeaderValue("action").Return("foo", nil)
+	mockAction.EXPECT().Process(mockMsg).Return(nil)
+
+	mockConn.EXPECT().AckMessage(mockMsg).Return(
+		errors.New("Something went wrong!"),
+	)
+	mockConn.EXPECT().AckMessage(mockMsg).Return(nil)
+
+	con := service.NewConsumer(mockConn)
+	con.AddAsyncAction(mockAction, "foo")
+
+	con.Listen()
+}
