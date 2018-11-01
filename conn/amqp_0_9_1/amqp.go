3fc71757683edd4448ec18e334621c9f4684b840
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-11-01 19:17:31 +0100

    Separated the functionality a bit

--- conn/amqp_0_9_1/amqp.go
+++ conn/amqp_0_9_1/amqp.go
@@ -2,8 +2,6 @@ package amqp_0_9_1
 
 import(
 	"github.com/streadway/amqp"
-	"errors"
-	"strings"
 )
 
 // Provides a couple of thin convenience wrappers around streadway's AMQP
@@ -33,62 +31,4 @@ func NewConn(url string) (*Connection, error) {
 		amqpConn: amqpConn,
 		amqpChan: amqpChan,
 	}, nil
-}
-
-func (c *Connection) SetTargetQueue(name string) error {
-	// We're gonna make a few assumptions here - since this lib is mainly
-	// meant to take the place of "traditional" HTTP-based microservices,
-	// we'll make the queue stick around forever
-	_, err := c.amqpChan.QueueDeclare(name, true, false, false, false, nil)
-
-	if (err != nil) {
-		c.queue = name
-	}
-
-	return err
-}
-
-type ExchangeType string
-
-const(
-	Direct	ExchangeType = "direct"
-	Fanout	ExchangeType = "fanout"
-	Topic	ExchangeType = "topic"
-	Headers	ExchangeType = "headers"
-)
-
-func (c *Connection) CreateAndBindExchange(
-	name string,
-	exchType ExchangeType,
-	routingKey string,
-) error {
-	if (c.queue == "") {
-		return errors.New(strings.Join([]string{
-			"Need to create a queue before attempting to bind ",
-			"it to an exchange",
-		}, ""))
-	}
-
-	// Like before, we're gonna make a few basic assumptions here just
-	// to make life simple
-	err := c.amqpChan.ExchangeDeclare(
-		name,
-		string(exchType),
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-
-	if (err != nil) {
-		return err
-	}
-
-	err = c.amqpChan.QueueBind(c.queue, routingKey, name, false,nil)
-	if (err != nil) {
-		return err
-	}
-
-	return nil
 }
---
+++ conn/amqp_0_9_1/topology.go
@@ -0,0 +1,64 @@
+package amqp_0_9_1
+
+import(
+	"errors"
+	"strings"
+)
+
+func (c *Connection) SetTargetQueue(name string) error {
+	// We're gonna make a few assumptions here - since this lib is mainly
+	// meant to take the place of "traditional" HTTP-based microservices,
+	// we'll make the queue stick around forever
+	_, err := c.amqpChan.QueueDeclare(name, true, false, false, false, nil)
+
+	if (err != nil) {
+		c.queue = name
+	}
+
+	return err
+}
+
+type ExchangeType string
+
+const(
+	Direct	ExchangeType = "direct"
+	Fanout	ExchangeType = "fanout"
+	Topic	ExchangeType = "topic"
+	Headers	ExchangeType = "headers"
+)
+
+func (c *Connection) CreateAndBindExchange(
+	name string,
+	exchType ExchangeType,
+	routingKey string,
+) error {
+	if (c.queue == "") {
+		return errors.New(strings.Join([]string{
+			"Need to create a queue before attempting to bind ",
+			"it to an exchange",
+		}, ""))
+	}
+
+	// Like before, we're gonna make a few basic assumptions here just
+	// to make life simple
+	err := c.amqpChan.ExchangeDeclare(
+		name,
+		string(exchType),
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if (err != nil) {
+		return err
+	}
+
+	err = c.amqpChan.QueueBind(c.queue, routingKey, name, false,nil)
+	if (err != nil) {
+		return err
+	}
+
+	return nil
+}
