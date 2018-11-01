dc1377b35feac29f650a7ec102fe39ebea416bad
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-11-01 22:51:37 +0100

    Finished the implementation of the message type

--- conn/amqp_0_9_1/message.go
+++ conn/amqp_0_9_1/message.go
@@ -2,6 +2,8 @@ package amqp_0_9_1
 
 import(
 	"github.com/streadway/amqp"
+	"errors"
+	"strings"
 )
 
 type message struct{
@@ -12,4 +14,26 @@ func newMessage(d amqp.Delivery) message {
 	return message{
 		inner: d,
 	}
+}
+
+func (m message) GetBody() []byte {
+	return m.inner.Body
+}
+
+// I'm not _100%_ happy with how this works because the headers can be
+// recursive, but I'm not sure how best to deal with it for now
+func (m message) GetHeaderValue(headerName string) (interface{}, error) {
+	if value, ok := m.inner.Headers[headerName]; ok {
+		return value, nil
+	}
+
+	return nil, errors.New(strings.Join([]string{
+		"Header \"",
+		headerName,
+		"\" not found",
+	}, ""))
+}
+
+func (m message) GetAllHeaders() map[string]interface{} {
+	return m.inner.Headers
 }
