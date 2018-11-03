36327f2f2a78f9e0fd68c79c960a739069e25469
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-11-03 10:51:41 +0100

    Added a test for creating exchanges

--- conn/amqp_0_9_1/topology_test.go
+++ conn/amqp_0_9_1/topology_test.go
@@ -62,3 +62,46 @@ func TestTargetQueueFailure(t *testing.T) {
 
 	assert.NotNil(t, err)
 }
+
+func TestCreateAndBindSuccess(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockChan := q_test_mocks.NewMockAMQPChan(mockCtrl)
+	mockConn := q_test_mocks.NewMockAMQPConn(mockCtrl)
+
+	mockConn.EXPECT().Channel().Return(mockChan, nil)
+	mockChan.EXPECT().Qos(1, 0, false).Return(nil)
+
+	mockChan.EXPECT().QueueDeclare(
+		"foobar",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	).Return(amqp.Queue{}, nil)
+	mockChan.EXPECT().ExchangeDeclare(
+		"barbaz",
+		"fanout",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	).Return(nil)
+	mockChan.EXPECT().QueueBind(
+		"foobar",
+		"",
+		"barbaz",
+		false,
+		nil,
+	).Return(nil)
+
+	conn, _ := amqp_0_9_1.NewConn(mockConn)
+	conn.SetTargetQueue("foobar")
+
+	err := conn.CreateAndBindExchange("barbaz", amqp_0_9_1.Fanout, "")
+
+	assert.Nil(t, err)
+}
