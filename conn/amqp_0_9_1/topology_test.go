4acf5e59595c6820a52ad87a19fa9ae44355f9c7
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-11-03 10:40:47 +0100

    Started adding tests for the topology stuff

---
+++ conn/amqp_0_9_1/topology_test.go
@@ -0,0 +1,64 @@
+package amqp_0_9_1_test
+
+import(
+	"github.com/NGTOne/warren/conn/amqp_0_9_1"
+
+	"testing"
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/streadway/amqp"
+	q_test_mocks "github.com/NGTOne/warren/test_mocks/conn/amqp_0_9_1"
+)
+
+func TestTargetQueueSuccess(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockChan := q_test_mocks.NewMockAMQPChan(mockCtrl)
+	mockConn := q_test_mocks.NewMockAMQPConn(mockCtrl)
+
+	mockConn.EXPECT().Channel().Return(mockChan, nil)
+	mockChan.EXPECT().Qos(1, 0, false).Return(nil)
+
+	mockChan.EXPECT().QueueDeclare(
+		"foobar",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	).Return(amqp.Queue{}, nil)
+
+	conn, _ := amqp_0_9_1.NewConn(mockConn)
+
+	err := conn.SetTargetQueue("foobar")
+
+	assert.Nil(t, err)
+}
+
+func TestTargetQueueFailure(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockChan := q_test_mocks.NewMockAMQPChan(mockCtrl)
+	mockConn := q_test_mocks.NewMockAMQPConn(mockCtrl)
+
+	mockConn.EXPECT().Channel().Return(mockChan, nil)
+	mockChan.EXPECT().Qos(1, 0, false).Return(nil)
+
+	mockChan.EXPECT().QueueDeclare(
+		"foobar",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	).Return(amqp.Queue{}, errors.New("Something went wrong!"))
+
+	conn, _ := amqp_0_9_1.NewConn(mockConn)
+
+	err := conn.SetTargetQueue("foobar")
+
+	assert.NotNil(t, err)
+}
