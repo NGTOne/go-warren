34598c6ea1592a909508b3f5aa1f20e53bd7b5f0
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-11-30 21:02:24 +0100

    Added header tests

---
+++ conn/amqp_0_9_1/message_test.go
@@ -0,0 +1,66 @@
+package amqp_0_9_1
+
+import (
+	"github.com/streadway/amqp"
+	"github.com/stretchr/testify/assert"
+	"time"
+	"testing"
+)
+
+func TestGetBody(t *testing.T) {
+	m := message{
+		inner: amqp.Delivery{
+			Body: []byte("It's the body!"),
+		},
+	}
+
+	assert.Equal(t, []byte("It's the body!"), m.GetBody())
+}
+
+func TestGetHeaderValue(t *testing.T) {
+	m := message{
+		inner: amqp.Delivery{
+			ContentType: "text/json",
+			ContentEncoding: "base64",
+			DeliveryMode: 1,
+			Priority: 1,
+			CorrelationId: "f00b4r",
+			ReplyTo: "somewhere",
+			Expiration: "It's gonna expire",
+			MessageId: "Something",
+			Timestamp: time.Unix(100, 0),
+			Type: "Some type",
+			UserId: "Someone",
+			AppId: "An app",
+			Headers: map[string]interface{}{
+				"some_header": 123,
+			},
+		},
+	}
+
+	tests := []struct{
+		header string
+		result interface{}
+	}{
+		{"ContentType", "text/json"},
+		{"ContentEncoding", "base64"},
+		{"DeliveryMode", uint8(1)},
+		{"Priority", uint8(1)},
+		{"CorrelationId", "f00b4r"},
+		{"ReplyTo", "somewhere"},
+		{"Expiration", "It's gonna expire"},
+		{"MessageId", "Something"},
+		{"Timestamp", time.Unix(100, 0)},
+		{"Type", "Some type"},
+		{"UserId", "Someone"},
+		{"AppId", "An app"},
+		{"some_header", 123},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.header, func (t *testing.T) {
+			result, _ := m.GetHeaderValue(tt.header)
+			assert.Equal(t, tt.result, result)
+		})
+	}
+}
