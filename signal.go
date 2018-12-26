72dcad27f4f6993293073f1a4c1e2cfcf195190b
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2018-12-26 14:32:53 +0100

    Added a basic type for processing signals

---
+++ signal.go
@@ -0,0 +1,52 @@
+package warren
+
+import(
+	"os"
+	"os/signal"
+	"github.com/NGTOne/warren/sig_handler"
+)
+
+type signalProcessor struct {
+	handler		sig_handler.SignalHandler
+	targetSignals	[]os.Signal
+	caughtSignals	[]os.Signal
+	catcher		chan os.Signal
+}
+
+func newProcessor() *signalProcessor {
+	p := &signalProcessor{
+		handler:	nil,
+		targetSignals:	[]os.Signal{},
+		caughtSignals:	[]os.Signal{},
+		catcher:	make(chan os.Signal),
+	}
+
+	go func() {
+		for {
+			sig := <-p.catcher
+			p.caughtSignals = append(p.caughtSignals, sig)
+		}
+	}()
+
+	return p
+}
+
+func (p *signalProcessor) setHandler (handler sig_handler.SignalHandler) {
+	p.handler = handler
+}
+
+func (p *signalProcessor) setTargetSignals(signals []os.Signal) {
+	p.targetSignals = signals
+}
+
+func (p *signalProcessor) enable() {
+	signal.Notify(p.catcher, p.targetSignals...)
+}
+
+func (p *signalProcessor) processSignals() {
+	p.handler.HandleSignals(p.caughtSignals)
+}
+
+func (p *signalProcessor) disable() {
+	signal.Stop(p.catcher)
+}
