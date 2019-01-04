dbd01914279880f944f0ca337f5e5cd5e2bdbcee
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2019-01-04 23:52:16 +0100

    Dropped the exiting handler in favour of one that panics

---
+++ sig_handler/panicking.go
@@ -0,0 +1,23 @@
+package sig_handler
+
+import(
+	"os"
+)
+
+type panickingHandler struct{
+	panicMsgs map[os.Signal]string
+}
+
+func NewPanickingHandler(panicMsgs map[os.Signal]string) panickingHandler {
+	return panickingHandler{
+		panicMsgs: panicMsgs,
+	}
+}
+
+func (h panickingHandler) handleSignals(sigs []os.Signal) {
+	for _, sig := range sigs {
+		if msg, ok := h.panicMsgs[sig]; ok {
+			panic(msg)
+		}
+	}
+}
